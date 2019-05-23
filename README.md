ba247af8c628af54227c4b19598208e2fb071525
melaniehoff <m@melanie-hoff.com>
2019-05-23 04:04:34 -0400

    tooltip for interaction and project page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # generative-tarot
 Generative Tarot is a 3 card reading website using major arcana cards designed and coded by students in my Rutgers Design 2B class in Spring 19
+
+[Online Reader](https://melanie-hoff.com/tarot/)
+
+[Generative Tarot Project Description](https://melanie-hoff.com/tarot/project/)
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-    console.log( "ready!" );
+
+
     var cards = [
     // structure:---->  console.log(cards[0].card); = the fool
     {
@@ -7,154 +8,176 @@
       website: "http://artfiles.rutgers.edu/~maiwaldt@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/2fsGq6inE",
       code: "https://editor.p5js.org/melaniehoff/sketches/2fsGq6inE",
-      interaction: "<span><img class='click' src='assets/mouse.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'>",
+      interactionText: "Move mouse"
     },
     {
       card: "The Magician",
       website: "http://artfiles.rutgers.edu/~manansala@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/tXz9E7jd0",
       code: "https://editor.p5js.org/melaniehoff/sketches/tXz9E7jd0",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'> A B C D E</span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'> A B C D E",
+      interactionText: "Move mouse, click, press A, B, C, D, E"
     },
     {
       card: "The High Priestess",
       website: "http://artfiles.rutgers.edu/~farah@art.rutgers.edu/tarot",
       sketch: "https://editor.p5js.org/melaniehoff/embed/cbjTgdqFM",
       code: "https://editor.p5js.org/melaniehoff/sketches/cbjTgdqFM",
-      interaction: "<img class='click' src='assets/click.png'>"
+      interaction: "<img class='click' src='assets/click.png'>",
+      interactionText: "Click"
     },
     {
       card: "The Empress",
       website: "http://artfiles.rutgers.edu/~feerrar@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/pKXvxBY8f",
       code: "https://editor.p5js.org/melaniehoff/sketches/pKXvxBY8f",
-      interaction: "<img class='click' src='assets/click.png'>"
+      interaction: "<img class='click' src='assets/click.png'>",
+      interactionText: "Click"
     },
     {
       card: "The Emperor",
       website: "http://artfiles.rutgers.edu/~feerrar@art.rutgers.edu/tarot%202/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/FUKeNgxvC",
       code: "https://editor.p5js.org/melaniehoff/sketches/FUKeNgxvC",
-      interaction: "<img class='click' src='assets/click.png'>"
+      interaction: "<img class='click' src='assets/click.png'>",
+      interactionText: "Click"
     },
     {
       card: "The Hierophant", //TODO
-      website: "",
+      website: "https://melanie-hoff.com/tarot/hierophant/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/MtfvWmYz6",
       code: "https://editor.p5js.org/melaniehoff/sketches/MtfvWmYz6",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'>",
+      interactionText: "Move mouse to play pong, also click"
     },
     {
       card: "The Lovers", //TODO
-      website: "http://www.aeclectic.net/tarot/learn/meanings/lovers.shtml",
+      website: "https://melanie-hoff.com/tarot/lovers",
       sketch: "https://editor.p5js.org/melaniehoff/embed/poq8yuXSq",
       code: "https://editor.p5js.org/melaniehoff/sketches/poq8yuXSq",
-      interaction: "<span><img class='click' src='assets/click.png'> R</span>"
+      interaction: "<span><img class='click' src='assets/click.png'> R</span>",
+      interactionText: "Click, press R"
     },
     {
       card: "The Chariot",
       website: "http://artfiles.rutgers.edu/~macneill@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/OgyCbc4-z",
       code: "https://editor.p5js.org/melaniehoff/sketches/OgyCbc4-z",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'>",
+      interactionText: "Move mouse, click"
     },
     {
       card: "Strength",
       website: "http://artfiles.rutgers.edu/~wangy@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/r3eekK1O0",
       code: "https://editor.p5js.org/melaniehoff/sketches/r3eekK1O0",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'>",
+      interactionText: "Click + hold"
     },
     {
       card: "The Hermit", //TODO -- fred's https://editor.p5js.org/fredkq1/sketches/AUvwFDPvy
       website: "http://artfiles.rutgers.edu/~quayenortey@art.rutgers.edu/tarot_2/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/5h8TKriVb",
       code: "https://editor.p5js.org/melaniehoff/sketches/5h8TKriVb",
-      interaction: "<span><img class='click' src='assets/click.png'> [SPACE] E F</span>"
+      interaction: "<img class='click' src='assets/click.png'> [SPACE] E F",
+      interactionText: "Click, press spacebar, E key, F key"
     },
     {
       card: "Wheel of Fortune", //TODO
-      website: "",
+      website: "https://melanie-hoff.com/tarot/wheeloffortune",
       sketch: "https://editor.p5js.org/melaniehoff/embed/H7E65rqDb",
       code: "https://editor.p5js.org/melaniehoff/sketches/H7E65rqDb",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'>",
+      interactionText: "Move mouse"
     },
     {
       card: "Justice",
       website: "http://artfiles.rutgers.edu/~finn@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/8gZLw_VYr",
       code: "https://editor.p5js.org/melaniehoff/sketches/8gZLw_VYr",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'>",
+      interactionText: "Move mouse, click"
     },
     {
       card: "The Hanged Man",
       website: "http://artfiles.rutgers.edu/~quayenortey@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/C2BJdFo9_",
       code: "https://editor.p5js.org/melaniehoff/sketches/C2BJdFo9_",
-      interaction: "<img class='click' src='assets/click.png'>"
+      interaction: "<img class='click' src='assets/click.png'>",
+      interactionText: "Move mouse, click + listen"
     },
     {
       card: "Death",
       website: "http://artfiles.rutgers.edu/~wangy@art.rutgers.edu/tarot/death",
       sketch: "https://editor.p5js.org/melaniehoff/embed/PZ0GnLKyT",
       code: "https://editor.p5js.org/melaniehoff/sketches/PZ0GnLKyT",
-      interaction: "<span><img class='click' src='assets/mouse.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'>",
+      interactionText: "Move mouse"
     },
     {
       card: "Temperance", //TODO
-      website: "",
+      website: "https://melanie-hoff.com/tarot/temperence/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/ml_VPt1xc",
       code: "https://editor.p5js.org/melaniehoff/sketches/ml_VPt1xc",
-      interaction: "<span><img class='click' src='assets/click.png'></span>"
+      interaction: "<img class='click' src='assets/click.png'>",
+      interactionText: "Click"
     },
     {
       card: "The Devil", //TODO
-      website: "",
+      website: "https://melanie-hoff.com/tarot/devil",
       sketch: "https://editor.p5js.org/melaniehoff/embed/bbs_vgFxZ",
       code: "https://editor.p5js.org/melaniehoff/sketches/bbs_vgFxZ",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>"
+      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'></span>",
+      interactionText: "Move mouse, click, click + hold"
     },
     {
       card: "The Tower",
       website: "http://artfiles.rutgers.edu/~lozano@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/GFKaSQWhC",
       code: "https://editor.p5js.org/melaniehoff/sketches/GFKaSQWhC",
-      interaction: "<span><img class='click' src='assets/key.png'></span>"
+      interaction: "<img class='click' src='assets/key.png'>",
+      interactionText: "Press any key"
     },
     {
       card: "The Star",
       website: "http://artfiles.rutgers.edu/~mi@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/wS2zJaZXC",
       code: "https://editor.p5js.org/melaniehoff/sketches/wS2zJaZXC",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/key.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/key.png'>",
+      interactionText: "Move mouse, press any key"
     },
     {
       card: "The Moon",
       website: "http://artfiles.rutgers.edu/~leonard@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/9HQ_TLpGg",
       code: "https://editor.p5js.org/melaniehoff/sketches/9HQ_TLpGg",
-      interaction: "<span><img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'> <img class='click' src='assets/key.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'> <img class='click' src='assets/click.png'> <img class='click' src='assets/key.png'>",
+      interactionText: "Move mouse"
     },
     {
       card: "The Sun",
       website: "http://artfiles.rutgers.edu/~espinoza@art.rutgers.edu/tarot/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/v1YzgP6-u",
       code: "https://editor.p5js.org/melaniehoff/sketches/v1YzgP6-u",
-      interaction: "<img class='click' src='assets/space.png'>"
+      interaction: "<img class='click' src='assets/space.png'>",
+      interactionText: ""
     },
     {
       card: "Judgement", //TODO copy to my p5
-      website: "",
+      website: "https://melanie-hoff.com/tarot/judgement/",
       sketch: "https://editor.p5js.org/melaniehoff/embed/LiEPr44Id",
       code: "https://editor.p5js.org/melaniehoff/sketches/LiEPr44Id",
-      interaction: "<img class='click' src='assets/space.png'>"
+      interaction: "<img class='click' src='assets/space.png'>",
+      interactionText: ""
     },
     {
       card: "The World",
       website: "http://artfiles.rutgers.edu/~noor@art.rutgers.edu/tarot",
       sketch: "https://editor.p5js.org/melaniehoff/embed/oUL56jqc1",
       code: "https://editor.p5js.org/melaniehoff/sketches/oUL56jqc1",
-      interaction: "<span><img class='click' src='assets/mouse.png'></span>"
+      interaction: "<img class='click' src='assets/mouse.png'>",
+      interactionText: "Move mouse"
     }
     ];
 
@@ -162,12 +185,16 @@ displayCards(cards);
 
 
 
-
-
 //FUNCTIONS
 
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ alert("Generative Tarot is MUCH better viewed on desktop.\nYou'll be missing out on the card interactions.\nContinue anyway?");
+
+}
+
+
 function displayCards(cards) {
-  console.log("displayed the cards??????????");
+  console.log("Check out this console log!");
 
   // Shuffle array
   const shuffledCards = shuffle(cards);
@@ -200,6 +227,12 @@ function displayCards(cards) {
   $("#interaction-2").html(shuffledThree[1].interaction);
 
   $("#interaction-3").html(shuffledThree[2].interaction);
+
+  $("#interactionText-1").html(shuffledThree[0].interactionText);
+
+  $("#interactionText-2").html(shuffledThree[1].interactionText);
+
+  $("#interactionText-3").html(shuffledThree[2].interactionText);
 
   //Show the iframes
   loadIframe("one", shuffledThree[0].sketch);
